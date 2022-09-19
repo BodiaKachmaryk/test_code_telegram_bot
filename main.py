d829bb8146c49b88e872f00d2d0ca96f28b5618d
Bogdan <bogdankacmarik@gmail.com>
2022-09-19 19:39:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 from level_select import show_levels_select
 from game_manager import GameManager
 from game_state import GameState
+from telegram import Update 
+from telegram.ext import Updater, Filters, CommandHandler, MessageHandler, CallbackContext 
+ 
+token = "5403480122:AAED8OVpeusNYxgR4BGb-fRZLnR35e4XRd8" 
+updater = Updater(token, use_context=True) 
+dispatcher = updater.dispatcher 
+ 
+def start_handler(update: Update, context: CallbackContext): 
+    context.bot.send_message(chat_id=update.effective_chat.id,  text=f"Hello, {update.effective_chat.first_name}!") 
+ 
+def message_handler(update: Update, context: CallbackContext): 
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text) 
+ 
+start_command_handler = CommandHandler("start", start_handler) 
+dispatcher.add_handler(start_command_handler) 
+ 
+text_handler = MessageHandler(Filters.text & ~Filters.command, message_handler) 
+dispatcher.add_handler(text_handler) 
+ 
+# Start bot 
+updater.start_polling()
 
 def intro():
     """Function asks for user name to play game."""
